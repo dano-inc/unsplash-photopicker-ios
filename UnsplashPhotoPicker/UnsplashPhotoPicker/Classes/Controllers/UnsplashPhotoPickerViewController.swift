bcd26fd21ae024822ffed25a2a3e671aebfb621e
bongzniak <bongzniak@dano.me>
2022-07-20 17:35:32 +0900

    💄 검색결과가 없을 경우 UI 수정

--- UnsplashPhotoPicker/UnsplashPhotoPicker/Classes/Controllers/UnsplashPhotoPickerViewController.swift
+++ UnsplashPhotoPicker/UnsplashPhotoPicker/Classes/Controllers/UnsplashPhotoPickerViewController.swift
@@ -360,8 +360,18 @@ extension UnsplashPhotoPickerViewController: PagedDataSourceDelegate {
     }
 
     func dataSource(_ dataSource: PagedDataSource, didFetch items: [UnsplashPhoto]) {
-        guard items.count > 0 else { return }
-        guard dataSource.items.count > 0 else {
+        guard items.count > 0
+        else {
+            DispatchQueue.main.async {
+                self.spinner.stopAnimating()
+                self.showEmptyView(with: .noResults)
+            }
+            
+            return
+        }
+        
+        guard dataSource.items.count > 0
+        else {
             DispatchQueue.main.async {
                 self.spinner.stopAnimating()
                 self.showEmptyView(with: .noResults)
